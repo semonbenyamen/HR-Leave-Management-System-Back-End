15e4598e5f46779ff7de241037689c06ce59df0a
Hazem <abdelazez.hazem@gmail.com>
2026-05-16 10:21:46 +0300

    Update phase 3

--- routes/leaveRequestRoutes.js
+++ routes/leaveRequestRoutes.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const router = express.Router();
-const { createLeaveRequest, updateLeaveStatus, getMyLeaves, getAllLeaves } = require('../Controllers/leaveRequestController');
-const { verifyTocken, verifyHR } = require('../middleware/verifyToken_role');
 
+const {
+  createLeaveRequest,
+  updateLeaveStatus,
+  getMyLeaves,
+  getAllLeaves,
+  getprofiledata,
+  gettotalleaves
+} = require('../Controllers/leaveRequestController');
+
+const {
+  verifyTocken,
+  verifyHR
+} = require('../middleware/verifyToken_role');
+
+// user routes
 router.get('/my', verifyTocken, getMyLeaves);
 
-router.post('/', verifyTocken, createLeaveRequest);
+
+router.post('/', createLeaveRequest);
+
+
 
 router.get('/', verifyHR, getAllLeaves);
-
 router.put('/:id/status', verifyHR, updateLeaveStatus);
+
+/*
+
+router.get('/profiledata', verifyTocken, getprofiledata);*/
+
+router.get('/profiledata/:userId', getprofiledata);
 
 module.exports = router;
